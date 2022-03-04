fbb13b34741da745de641fbc2d6d040949c81321
yanh1993 <891367701@qq.com>
2022-03-04 22:21:49 +0800

    first commit

---
+++ Aliyun.Log.Core.Client/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aliyun.Api.LogService;
+using Aliyun.Log.Core.Client.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Aliyun.Log.Core.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public static ILogServiceClient client { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.Configure<SlsOptions>(Configuration.GetSection("SlsOptions"));
+            var settings = services.BuildServiceProvider().GetService<IOptions<SlsOptions>>().Value;
+
+            //注入阿里云(主要用来写入日志,二次封装)
+            services.AddAliyunLog(m =>
+            {
+                m.AccessKey = settings.AccessKey;
+                m.AccessKeyId = settings.AccessKeyId;
+                m.Endpoint = settings.Host;
+                m.Project = settings.Project;
+                m.LogStoreName = settings.LogstoreName;
+            });
+
+            //使用阿里云SDK提供的方式(主要用来查询日志)
+            client = LogServiceClientBuilders.HttpBuilder
+          .Endpoint(settings.Host, settings.Project)
+          .Credential(settings.AccessKeyId, settings.AccessKeyId)
+          .Build();
+
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                //忽略循环引用
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                //不使用驼峰样式的key
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                //设置时间格式
+                options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
